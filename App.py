f399bf71f10692798450a7cb1297e1818d2a0ba0
Ilargi12 <bplewnia99@gmail.com>
2020-05-30 17:55:55 +0200

    Added to GlobalStats refresh button

--- App.py
+++ App.py
@@ -3,7 +3,3 @@ from visualization.GUI import GUI
 if __name__ == "__main__":
     app = GUI()
     app.mainloop()
-
-# import requests
-# r = requests.get(url='http://127.0.0.1:5000/statistics')
-# print(r.json())
--- visualization/GlobalStats.py
+++ visualization/GlobalStats.py
@@ -9,30 +9,27 @@ class GlobalStats(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
-        self.service = GlobalStatsService()
+        self.service = None
         self.configure(bg="#f9f8fd")
         self.style = ttk.Style()
         self.style.configure("Treeview.Heading", font=("Comic Sans MS", 20, "bold"))
         self.menu_photo = tk.PhotoImage(file=r"img/menu.png")
         self.top_ten_players_photo = tk.PhotoImage(file=r"img/top_ten_players.png")
         self.wpm_stats_photo = tk.PhotoImage(file=r"img/wpm_stats.png")
+        self.refresh_photo = tk.PhotoImage(file=r"img/refresh.png")
 
+        self.top_ten_players = None
         self.table = None
         self.graph = None
-
-        tk.Label(self, bg="#f9f8fd", image=self.top_ten_players_photo).pack(pady=(10, 0))
-        self.show_table()
-
-        tk.Label(self, bg="#f9f8fd", image=self.wpm_stats_photo).pack()
-        self.show_donut_graph()
-
-        tk.Button(self, image=self.menu_photo, bg="#f9f8fd", borderwidth=0,
-                  command=lambda: controller.show_frame("MainMenu")).pack()
+        self.wpm_stats = None
+        self.menu_button = None
+        self.refresh_button = None
+        self.display_visualization()
 
     def show_table(self):
         player_list = self.service.get_top_ten_players()
         cols = ['Position', 'Name', 'WPM', 'CPM']
-        self.table = ttk.Treeview(self, columns=cols, show='headings')
+        self.table = ttk.Treeview(self, columns=cols, show='headings', selectmode='none')
         for col in cols:
             self.table.heading(col, text=col)
 
@@ -44,12 +41,11 @@ class GlobalStats(tk.Frame):
                 self.table.insert("", "end", values=(i, player['name'], '%.3f'%player['wpm'], '%.3f'%player['cpm']),
                                   tags=('odd_row', ))
             else:
-                self.table.insert("", "end", values=(i, player['name'], '%.3f'%player['wpm'], '%.3f'%player['cpm']),
+                self.table.insert("", "end", values=(i, player['name'], '%.3f' % player['wpm'], '%.3f' % player['cpm']),
                                   tags=('even_row', ))
 
         self.table.tag_configure('odd_row', background='#c1c3e8')
         self.table.tag_configure('even_row', background='#666ccd')
-
         self.table.pack()
 
     def show_donut_graph(self):
@@ -67,6 +63,37 @@ class GlobalStats(tk.Frame):
         self.graph = FigureCanvasTkAgg(p, self)
         self.graph.get_tk_widget().pack(fill='x')
 
+    def display_visualization(self):
+        self.service = GlobalStatsService()
+        self.top_ten_players = tk.Label(self, bg="#f9f8fd", image=self.top_ten_players_photo)
+        self.top_ten_players.pack()
+
+        self.show_table()
+
+        self.wpm_stats = tk.Label(self, bg="#f9f8fd", image=self.wpm_stats_photo)
+        self.wpm_stats.pack()
+
+        self.show_donut_graph()
+
+        self.menu_button = tk.Button(self, image=self.menu_photo, bg="#f9f8fd", borderwidth=0,
+                                     command=lambda: self.controller.show_frame("MainMenu"))
+        self.menu_button.place(x=200, y=620)
+
+        self.refresh_button = tk.Button(self, image=self.refresh_photo, bg="#f9f8fd", borderwidth=0,
+                                        command=lambda: self.refresh())
+        self.refresh_button.place(x=600, y=620)
+
+    def delete_visualization(self):
+        self.top_ten_players.destroy()
+        self.table.destroy()
+        self.graph.get_tk_widget().destroy()
+        self.wpm_stats.destroy()
+        self.menu_button.destroy()
+        self.refresh_button.destroy()
+
+    def refresh(self):
+        self.delete_visualization()
+        self.display_visualization()
 
 
 
